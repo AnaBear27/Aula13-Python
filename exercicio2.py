205e45e93ec1e87914cc7add5050586aa50cd152
AnaBear27 <anabeatriz6878@gmail.com>
2025-03-25 09:54:41 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 12, 9]
+
+plt.plot(x, y)
+
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+plt.title('Gráfico de Linha')
+
+plt.show()
